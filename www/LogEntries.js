1f5804e4ba3b568deb34265a4069704eb44ed296
Mevin Dhunnooa <mevin.dhunnooa@sc-mauritius.com>
2018-04-10 13:30:19 +0400

    fixed incorrect method name

--- www/LogEntries.js
+++ www/LogEntries.js
@@ -3,6 +3,6 @@ module.exports = {
         cordova.exec(successCallback, errorCallback, "LogEntries", "init", [token]);
     }, 
     log: function (message, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "LogEntries", "logEvent", [message]);
+        cordova.exec(successCallback, errorCallback, "LogEntries", "log", [message]);
     }
 };
